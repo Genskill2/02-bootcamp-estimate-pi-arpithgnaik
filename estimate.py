cc2d857b97dd02a941f8352eaa4a87a83d7a5c43
arpithgnaik <arpithbdk6@gmail.com>
2021-05-31 22:19:29 +0530

    Functions-wallis and monte_carlo is added

--- estimate.py
+++ estimate.py
@@ -1,23 +1,52 @@
 import math
 import unittest
+import random
+
+def wallis(i):
+		pro=1
+		for n in range(1,i+1):
+			pro=pro*(4*n*n)/(4*n*n-1)
+
+		#return PI value
+		return pro*2
+
 
 class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
 
+	def test_low_iters(self):
+		for i in range(0, 5):
+			pi = wallis(i)
+			self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+			
+	def test_high_iters(self):
+		for i in range(500, 600):
+			pi = wallis(i)
+			self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+def monte_carlo(n):
+	circle=0
+	dist=0
+
+	for i in range(1,n+1):
+		#Genarte Random (x,y) cordinates
+		x=random.random()		
+		y=random.random()
+
+		#Calculate distance from origin (0,0) to random point (x,y)
+		dist=((x*x)+(y*y))**0.5
+
+		#if distance <=1, random point is inside the circle
+		if(dist<=1):
+			circle=circle+1
+
+	#return PI value
+	return 4*circle/n
 
 class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
+		
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
@@ -26,7 +55,7 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+		
+	
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
